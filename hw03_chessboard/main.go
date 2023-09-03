39dcac45e4037ded789d946e4954da52095f7a8e
EAD <e.a.dubina@gmail.com>
2023-09-03 16:01:23 +0300

    HW_002

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Println("ЗАДАЙТЕ РАЗМЕР ДОСКИ:")
+	fmt.Scanf("%d", &size)
+	fmt.Println()
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			fmt.Print("# ")
+		}
+		fmt.Println()
+	}
 }
