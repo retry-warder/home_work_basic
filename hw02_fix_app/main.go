d964c2b23acfd2eeccb629dce26b655dd66f26c5
EAD <e.a.dubina@gmail.com>
2023-09-03 16:28:03 +0300

    HW_001_1

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,10 +1,11 @@
 package init
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
 func init() {
@@ -19,6 +20,7 @@ func init() {
 	if len(path) == 0 {
 		path = "data.json"
 	} else {
+
 	}
 
 	staff, err = reader.ReadJSON(path, -1)
